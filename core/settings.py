b7384eb717072386d0a91715bb417055fa7d3ed7
damiso15 <damiso15@yahoo.com>
2021-09-16 11:58:31 +0100

    Bincom Programming Test: Deploying Question 1 on Heroku

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -142,3 +143,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+django_heroku.settings(locals())
