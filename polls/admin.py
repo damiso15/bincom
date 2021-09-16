e0431a9f24a284af18340adba941e7c052442d9c
damiso15 <damiso15@yahoo.com>
2021-09-16 11:53:43 +0100

    Bincom Programming Test: Just finished the Views and Urls for question 1

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import AgentName, AnnouncedLGAResults, AnnouncedPUResults, AnnouncedStateResults, AnnouncedWardResults, \
+    LGA, Party, PollingUnit, States, Ward
 
 # Register your models here.
+admin.site.register(AgentName)
+admin.site.register(AnnouncedLGAResults)
+admin.site.register(AnnouncedPUResults)
+admin.site.register(AnnouncedStateResults)
+admin.site.register(AnnouncedWardResults)
+admin.site.register(LGA)
+admin.site.register(Party)
+admin.site.register(PollingUnit)
+admin.site.register(States)
+admin.site.register(Ward)
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import AgentName, AnnouncedLGAResults, AnnouncedPUResults, AnnouncedStateResults, AnnouncedWardResults, \
+    LGA, Party, PollingUnit, States, Ward
 
 # Create your views here.
+
+
+class PollingUnitListView(generic.ListView):
+    model = PollingUnit
+    context_object_name = 'poll_list'
+    queryset = PollingUnit.objects.only('unique_id', 'polling_unit_name')
+    template_name = 'index.html'
+
+
+def polling_unit_detail_view(request, pk):
+    poll = get_object_or_404(PollingUnit, pk=pk)
+    abbrev = AnnouncedPUResults.objects.all().filter(polling_unit_unique_id=pk)
+    print(abbrev)
+    context = {
+        'poll': poll,
+        'abbreviations': abbrev,
+    }
+    print(poll, 'this is working')
+    return render(request, 'pollingunit_detail.html', context)
+
